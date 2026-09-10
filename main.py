4ad3fbe272a29d7edfd1faa28c92e0b55efdfee8
samirasyn <samiraseysenbek0@gmail.com>
2026-09-10 17:41:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from groq import Groq
+from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 api_key = os.getenv("GROQ_API_KEY")
 
 client = Groq(api_key=api_key)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 
 class PromptRequest(BaseModel):
     prompt: str
